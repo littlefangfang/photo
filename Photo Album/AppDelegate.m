478419f34c20bb38e2f2f43e2009197fdcdd5b9d
xinyue-0 <xinyue-0@xinyue-0demac-mini.local>
2016-10-12 19:42:10 +0800

    ...

--- Photo Album/AppDelegate.m
+++ Photo Album/AppDelegate.m
@@ -40,6 +40,14 @@
 
 - (void)applicationDidBecomeActive:(UIApplication *)application {
     // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
+    
+//    UIWindow *keyWindow = [UIApplication sharedApplication].keyWindow;
+//    UIViewController *vc = keyWindow.rootViewController;
+//    
+//    [keyWindow setWindowLevel:UIWindowLevelAlert];
+//    UIView *v = [[UIView alloc] initWithFrame:[UIScreen mainScreen].bounds];
+//    v.backgroundColor = [UIColor redColor];
+//    [keyWindow addSubview:v];
 }
 
 
